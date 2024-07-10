e4682f8c72c144ff9fd74b92dcebb1f4b20c7fcd
Beniih <albenir1989@gmail.com>
2024-07-10 16:47:42 -0300

    feat: user creation function

--- sistema-bancario-otimizado.py
+++ sistema-bancario-otimizado.py
@@ -1,35 +1,89 @@
+import textwrap
 from datetime import datetime
 
+def menu():
+    menu = """
+    =================== MENU ====================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [n]\tNova Conta
+    [u]\tNovo Usuário
+    [l]\tListar Contas
+    [q]\tSair
+
+    => """
+    return input(textwrap.dedent(menu))
+
 def criar_usuario(usuarios):
-    cpf = int(input("Informe o CPF (apenas números): "))
-    nome = str(input("Informe o nome do usuário: "))
-    data_nascimento = str(input("Informe a data de nascimento: "))
-    logradouro = str(input("Informe o logradouro (Rua, Av., etc): "))
-    nro = int(input("Informe o número (0 caso S/N): "))
-    bairro = str(input("Informe o bairro: "))
-    cidade = str(input("Informe a cidade: "))
-    estado = str(input("Informe a sigla do estado (ex.: SP, RJ): "))
+    while True:
+        try:
+            cpf = int(input("Informe o CPF (apenas números): "))
+            if len(str(cpf)) == 11:
+                break
+            else:
+                print("Por favor, insira um CPF válido.")
+            continue
+        except ValueError:
+            print("Entrada inválida.")
+
+    cpf = str(cpf)
+    usuario = verificar_cpf(usuarios, cpf=cpf)
+
+    if usuario:
+        print("!!! Já existe usuário cadastrado com este CPF!")
+        return
+
+    nome = input("Informe o nome do usuário: ")
+
+    while True:
+        try:
+            data = int(input("Informe a data de nascimento (dd-mm-aaaa): "))
+            if len(str(data)) == 8:
+                data = str(data)
+                break
+            else:
+                print("Por favor, insira uma data válida.")
+                continue
+        except ValueError:
+            print("Entrada inválida.")
+    data_nascimento = f"{data[:2]}/{data[2:4]}/{data[4:]}"
+
+    logradouro = input("Informe o logradouro (Rua, Av., etc): ")
+    nro = input("Informe o número: ")
+    bairro = input("Informe o bairro: ")
+    cidade = input("Informe a cidade: ")
+    while True:
+        estado = input("Informe as siglas do estado (ex.: SP, RJ): ")
+        if len(estado) == 2:
+            break
+        else:
+            print("Por favor, insira apenas as siglas do estado.")
+            continue
     endereco = f'{logradouro}, {nro} - {bairro} - {cidade}/{estado}'
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
     print(cpf,nome,data_nascimento,endereco)
 
-
 def verificar_cpf(usuarios,*,cpf):
-    pass
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
 
 def saque(*, valor, saldo, extrato, limite, saques_do_dia,limite_saque):
     if valor > saldo:
-        print("Operação falhou! Você não possui saldo sufuciente.")
+        print("!!! Operação falhou! Você não possui saldo sufuciente.")
     elif valor > limite:
-        print("Operação falhou! O valor do saque excedeu o limite.")
+        print("!!! Operação falhou! O valor do saque excedeu o limite.")
     elif saques_do_dia >= limite_saque:
-        print("Operação falhou! Número de saques foi excedido.")
+        print("!!! Operação falhou! Número de saques foi excedido.")
     elif valor > 0:
         saldo -= valor
         when = datetime.now().strftime('%d/%m/%Y %H:%M')
         extrato += f'{when} Saque:\t\tR$ {valor: .2f}-\n\n'
         saques_do_dia += 1
+        print("*** SAQUE REALIZADO COM SUCESSO ***")
     else:
-        print("Operação falhou! O valor informado é inválido.")
+        print("!!! Operação falhou! O valor informado é inválido.")
 
     return saldo, extrato, saques_do_dia
 
@@ -38,8 +92,9 @@ def deposito(valor, saldo, extrato, /):
         saldo += valor
         when = datetime.now().strftime('%d/%m/%Y %H:%M')
         extrato += f'{when} Deposito:\tR$ {valor: .2f}+\n\n'
+        print("*** DEPÓSITO REALIZADO COM SUCESSO ***")
     else:
-        print("Operação falhou! O valor informado é inválido. Tente novamente!")
+        print("!!! Operação falhou! O valor informado é inválido. Tente novamente!")
     return saldo, extrato
 
 def exibir_extrato(saldo,/,*,extrato):
@@ -49,15 +104,6 @@ def exibir_extrato(saldo,/,*,extrato):
     print("=============================================")
 
 def main():
-
-    menu = """
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [q] Sair
-
-    => """
-
     AGENCIA = "0001"
     LIMITE_SAQUES = 3
 
@@ -71,20 +117,36 @@ def main():
 
     while True:
         
-        opcao = input(menu)
+        opcao = menu()
 
         match opcao:
             case "d":
                 valor = float(input("Informe o valor do depósito: "))
-                saldo, extrato = deposito(valor, saldo, extrato)
+                saldo, extrato = deposito(
+                    valor,
+                    saldo,
+                    extrato
+                    )
             case "s":
                 valor = float(input("Informe o valor do saque: "))
-                saldo, extrato, saques_do_dia = saque(valor=valor, saldo=saldo, extrato=extrato, limite=limite, saques_do_dia=saques_do_dia, limite_saque=LIMITE_SAQUES)
+                saldo, extrato, saques_do_dia = saque(
+                    valor=valor,
+                    saldo=saldo,
+                    extrato=extrato,
+                    limite=limite,
+                    saques_do_dia=saques_do_dia,
+                    limite_saque=LIMITE_SAQUES
+                    )
             case "e":
-                exibir_extrato(saldo, extrato=extrato)
+                exibir_extrato(
+                    saldo,
+                    extrato=extrato
+                    )
             case "q":
                 break
+            case "u":
+                criar_usuario(usuarios)
             case _:
-                print("Operação inválida, por favor selecione novamente a operação desejada.")
+                print("!!! Operação inválida, por favor selecione novamente a operação desejada.")
 
 main()
